d975bfef66a25318b8e3a3a5538c3ba2ef8d90f7
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2022-09-21 22:30:21 +0200

    Add login

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -4,14 +4,16 @@ import { customLogger, WinstonLevel } from '../logger';
 import { User } from '../models/user';
 
 interface SignupConfig {
-	database?: User[]; // This is a temporal database
-	encryptFunc: (
+	database: User[]; // This is a temporal database
+	encryptFunc: (password: string, saltRounds: number) => Promise<string>;
+}
+
+interface LoginConfig {
+	database: User[];
+	decryptFunc: (
 		password: string,
-		saltRounds: number
-	) => Promise<{
-		salt: string;
-		hashedPassword: string;
-	}>;
+		hashedPassword: string
+	) => Promise<string | boolean>;
 }
 
 export const putSignup = (signupConfig: SignupConfig): ExpressRouteFunc => {
@@ -19,22 +21,58 @@ export const putSignup = (signupConfig: SignupConfig): ExpressRouteFunc => {
 		const id = req.body.id;
 		const email = req.body.email;
 		const password = req.body.password;
-		const { salt, hashedPassword } = await signupConfig.encryptFunc(
-			password,
-			10
-		);
+
+		const hashedPassword = await signupConfig.encryptFunc(password, 10);
+		// Later implement email is valid && password is secured enough
+		const users = signupConfig.database?.filter((user: User) => {
+			return user.email === email;
+		});
+		const isMatched = users.length !== 0;
+		if (isMatched) {
+			customLogger(WinstonLevel.ERROR, 'User already exist');
+			res.status(401).json({
+				message: 'invalid input',
+			});
+		}
+
 		const data: User = {
 			id: id,
 			email: email,
-			salt: salt,
 			hashedPassword: hashedPassword,
 		};
 		customLogger(WinstonLevel.INFO, 'Send sign-up data');
 		signupConfig.database?.push(data);
-		res.status(200).json(data);
+		res.status(201).json({
+			message: 'successfully signup',
+			data: data,
+		});
 	};
 };
 
-// export const postLogin = (): ExpressRouteFunc => {
-// 	return (req: Request, res: Response) => {};
-// };
+export const postLogin = (loginConfig: LoginConfig): ExpressRouteFunc => {
+	return async (req: Request, res: Response) => {
+		const email = req.body.email;
+		const password = req.body.password;
+		const users = loginConfig.database.filter((user: User) => {
+			return user.email === email;
+		});
+		const isValidEmail = users.length !== 0;
+		if (!isValidEmail) {
+			customLogger(WinstonLevel.ERROR, 'User not found');
+			res.status(401).json({ message: 'Invalid email or password' });
+		}
+		const hashedPassword = users[0].hashedPassword;
+		const isEqual = await loginConfig.decryptFunc(password, hashedPassword);
+		if (typeof isEqual === 'string') {
+			customLogger(WinstonLevel.ERROR, isEqual);
+			res.status(500).json({ error: isEqual });
+		}
+		if (!isEqual) {
+			customLogger(WinstonLevel.ERROR, 'Wrong Password');
+			res.status(401).json({ message: 'Invalid email or password' });
+		}
+		res.status(201).json({
+			message: 'successfully signup',
+		});
+	};
+};
