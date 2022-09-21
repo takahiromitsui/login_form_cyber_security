5926ad863720cb065959ca784d07fa7e2081ad78
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2022-09-21 20:49:49 +0200

    Use body-parser

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
+import bodyParser from 'body-parser';
 import express from 'express';
 import { customLogger, WinstonLevel } from './logger';
 import authRoutes from './routes/auth';
 
 const app = express();
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/auth', authRoutes);
 
 app.listen({ port: 8080 }, () => {
