8e26932d95e3281412276c3e380082c811af79e3
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2022-09-21 22:30:10 +0200

    Write decrypt Func

--- src/helpers/auth.ts
+++ src/helpers/auth.ts
@@ -7,3 +7,14 @@ export const encryptPassword = async (
 	const salt = await bcrypt.genSalt(saltRounds);
 	return await bcrypt.hash(password, salt);
 };
+
+export const decryptPassword = async (
+	password: string,
+	hashedPassword: string
+): Promise<string | boolean> => {
+	try {
+		return await bcrypt.compare(password, hashedPassword);
+	} catch (e) {
+		return (e as Error).message;
+	}
+};
