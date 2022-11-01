57a160fd12db24b78829202f452744fb2a0f7284
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2022-11-01 12:29:54 +0100

    Connect mongodb

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,25 @@ import cors from 'cors';
 import { customLogger, WinstonLevel } from './logger';
 import authRoutes from './routes/auth';
 import userRoutes from './routes/user';
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/auth', authRoutes);
 app.use('/user', userRoutes);
 
-app.listen({ port: 8080 }, () => {
-	customLogger(WinstonLevel.INFO, 'Running on port 8080');
-});
+mongoose
+	.connect(
+		`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fin4zdy.mongodb.net/security`
+	)
+	.then(res => {
+		app.listen(process.env.PORT || 8080);
+		customLogger(WinstonLevel.INFO, 'Running on port 8080');
+	})
+	.catch(e => {
+		const error = e as Error;
+		customLogger(WinstonLevel.ERROR, error.message);
+	});
